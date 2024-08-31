b64e3c993f2901361202fb4140e016390f7e2e26
katemba fahad <katsfahad008@gmail.com>
2024-08-31 11:15:10 +0300

    Work on get and login api for user model

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,16 +1,40 @@
-const {PrismaClient} = require('@prisma/client')
+const { PrismaClient, Role } = require("@prisma/client");
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const getUsers = async(req, res) =>{
-    const user = await prisma.user.findMany()
-    if(user){
-        res.json(user)
-    }else{
-        res.send('no users found')
+const getUsers = async (req, res) => {
+  const user = await prisma.user.findMany();
+  if (user) {
+    res.json(user);
+  } else {
+    res.send("no users found");
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (user) {
+    if (password === password) {
+      const token = await jwt.sign({ id: user.id, role: user.role }, process.env.SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      res.json({token, 'message':'success'})
+    } else {
+      res.send("password was incorrect");
     }
-}
+  } else {
+    res.send("user not found");
+  }
+};
 
 module.exports = {
-    getUsers
-}
+  getUsers,
+  loginUser
+};
--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -4,6 +4,8 @@ const userController = require('../Controllers/userController')
 
 userRouter.get('/', userController.getUsers)
 
+userRouter.post('/login', userController.loginUser)
+
 module.exports = {
     userRouter
 
